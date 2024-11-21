b2850893cb33261b72756f31e61e77ba3590d8fd
prade <prade@DESKTOP-K7Q0MAG>
2024-11-21 10:04:04 +0530

    Arithmetic Operator Program3

---
+++ inmakes_program/src/inmakes_program/Program3_ArithmeticOperator.java
@@ -0,0 +1,16 @@
+package inmakes_program;
+
+public class Program3_ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 5;
+		System.out.println("Addition is "+(a + b));
+		System.out.println("Substraction is "+(a - b));
+		System.out.println("Multiplication values is "+(a * b));
+		System.out.println("Division values "+(a / b));
+		System.out.println("WHat is this "+(a % b));
+	}
+
+}
