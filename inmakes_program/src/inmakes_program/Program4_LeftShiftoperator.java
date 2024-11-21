b25c164920f340f7434d77936b4dbb3e650e8d65
prade <prade@DESKTOP-K7Q0MAG>
2024-11-21 10:11:45 +0530

    Program4_LeftshiftOperator

---
+++ inmakes_program/src/inmakes_program/Program4_LeftShiftoperator.java
@@ -0,0 +1,12 @@
+package inmakes_program;
+
+public class Program4_LeftShiftoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Leftshift Opertor values is "+(10 << 2)); // 10*2Power(2) = 10*4 =40
+		System.out.println("Leftshift Opertor values is "+(10 << 4)); // 10*2power(4) = 10*16=160
+		System.out.println("Leftshift Opertor values is "+(20 << 5)); // 20*2power(5) = 20*32=640
+	}
+
+}
